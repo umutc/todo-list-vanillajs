cce769583811d1e7633af5479ea0675f47a68573
Umut ÇELİK <mail@umutcelik.com.tr>
2019-07-17 19:01:15 +0300

    fix: fixed the selector problem and click actions

--- to_do.js
+++ to_do.js
@@ -1,16 +1,63 @@
 var input = document.querySelector("input[type= 'text'");
-var ul = document.querySelector("ul");
-var lists = document.querySelectorAll("li");
-var spans = document.getElementByTagName("span");
-var addBtn = document.querySelector("#add-button");
-var deleteBtn = document.querySelector("#deleteall-button");
+var unorderedList = document.querySelector("ul");
+var unorderedListItems = document.querySelectorAll("li");
+var unorderedListItemSpans = document.querySelectorAll("li");
+var addBtn = document.getElementById("add-button");
+var deleteAllBtn = document.getElementById("deleteall-button");
+
+
+// [ Define Actions ]
+
+/**
+ * Add Todo
+ * Add todo to todo list
+ * 
+ * Note: () => unorderedList.removeChild(li)) is an arrow function;
+ */
+function addTodo() {
+	var value = input.value;
+	var li = document.createElement('li');
+	var textSpan = document.createElement('span');
+	var removeItemButton = document.createElement('button');
+
+
+	textSpan.textContent = value;
+	removeItemButton.textContent = "X";
+	removeItemButton.addEventListener("click", () => unorderedList.removeChild(li));
+
+	input.value = "";
+	li.appendChild(textSpan);
+	li.appendChild(removeItemButton);
+	unorderedList.appendChild(li);
+}
+
+/**
+ * Clear Todo List
+ * Clear the todo list(ul)
+ */
+function clearTodoList() {
+	var child = unorderedList.lastElementChild;
+	while (child) {
+		unorderedList.removeChild(child);
+		child = unorderedList.lastElementChild;
+	}
+}
 
 // Hepsini sil'e basıldığında yapılacaklar listesini temizlemek için.
-function deleteToDo(){
-	for(let span of spans){
-		span.addEventListener("click", function(){
+function deleteToDo() {
+	for (let span of document.getElementsByTagName("span")) {
+		span.addEventListener("click", function () {
 			span.parentElement.remove();
 			event.stopPropagation();
 		});
 	}
 }
+
+
+// [ Bind Actions ]
+addBtn.addEventListener("click", addTodo);
+deleteAllBtn.addEventListener("click", clearTodoList);
+
+
+// [ Clear Actions ]
+// in this section we should clear listeners on document close. But no matter right now.
